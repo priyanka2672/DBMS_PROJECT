27c34b5f5f370f59fa9d9743f62104f7abd0d0ec
shashwatha411 <shashwatha411@gmail.com>
2021-04-26 15:41:31 +0530

    updated

--- buyjavascript.js
+++ buyjavascript.js
@@ -85,6 +85,64 @@ function myjavas2()
         }
     }
 }
+function myjavas3()
+{
+     var person = prompt("Please enter your name");    
+    if(person != null)
+    {
+        var conf = prompt("Hello " + person + ", are you sure to buy Article : 405 Priced at 10,000 Indian Rupees. Type Yes / No.");
+        switch(conf){
+            case "Yes":
+                alert("Dear " + person + " you have successfully purchased Article 405");
+                break;
+            case "YES":
+                alert("Dear " + person + " you have successfully purchased Article 405");
+                break;
+            case "yes":
+                alert("Dear " + person + " you have successfully purchased Article 405");
+                break;
+            case "No":
+                alert("Dear " + person + " this Purchase is not successful!");
+                break;
+            case "NO":
+                alert("Dear " + person + " this Purchase is not successful!");
+                break;
+            case "no":
+                alert("Dear " + person + " this Purchase is not successful!");
+                break;
+            
+        }
+    }
+}
+function myjavas4()
+{
+     var person = prompt("Please enter your name");    
+    if(person != null)
+    {
+        var conf = prompt("Hello " + person + ", are you sure to buy Article : 407 Priced at 5,900 Indian Rupees. Type Yes / No.");
+        switch(conf){
+            case "Yes":
+                alert("Dear " + person + " you have successfully purchased Article 407");
+                break;
+            case "YES":
+                alert("Dear " + person + " you have successfully purchased Article 407");
+                break;
+            case "yes":
+                alert("Dear " + person + " you have successfully purchased Article 407");
+                break;
+            case "No":
+                alert("Dear " + person + " this Purchase is not successful!");
+                break;
+            case "NO":
+                alert("Dear " + person + " this Purchase is not successful!");
+                break;
+            case "no":
+                alert("Dear " + person + " this Purchase is not successful!");
+                break;
+            
+        }
+    }
+}
 function myjavas5()
 {
      var mine = prompt("Are you sure you want to register for Event 101?Type Yes/No.");    
@@ -130,6 +188,52 @@ function myjavas5()
                 alert("Registration Unsuccessful!");
                 break;
             
-        }
-    
+        }    
+}
+function myjavas6()
+{
+     var mine = prompt("Are you sure you want to register for Event 102?Type Yes/No.");    
+        switch(mine)
+        {
+            case "Yes":
+                var fname = prompt("Type in Your full name:");
+                if(fname != null)
+                {
+                    var phone=prompt("Enter your Phone Number");
+                    if(phone != null){
+                        alert("Dear "+ fname +" , you have successfully registered for event 102.");
+                    }
+                }
+                break;
+            case "YES":
+                var fname = prompt("Type in Your full name:");
+                if(fname != null)
+                {
+                    var phone=prompt("Enter your Phone Number");
+                    if(phone != null){
+                        alert("Dear "+ fname +" , you have successfully registered for event 102.");
+                    }
+                }
+                break;
+            case "yes":
+                var fname = prompt("Type in Your full name:");
+                if(fname != null)
+                {
+                    var phone=prompt("Enter your Phone Number");
+                    if(phone != null){
+                        alert("Dear "+ fname +" , you have successfully registered for event 102.");
+                    }
+                }
+                break;
+            case "No":
+                alert("Registration Unsuccessful!");
+                break;
+            case "NO":
+                alert("Registration Unsuccessful!");
+                break;
+            case "no":
+                alert("Registration Unsuccessful!");
+                break;
+            
+        }    
 }
